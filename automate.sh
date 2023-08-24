8955f624aef80f8b89ae182179e8a738c5c627de
Akash6222 <akashdubey25274@gmail.com>
2023-08-25 01:46:40 +0530

    added few more colors

--- automate.sh
+++ automate.sh
@@ -1,6 +1,23 @@
 #!/bin/env bash
 
+# Define text formatting variables
+bold=$(tput bold)
+normal=$(tput sgr0)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+
+# Function to display colored output
+print_message1() {
+    echo "${bold}${blue}$1${normal}"
+}
+
+print_message2() {
+    echo "${bold}${green}$1${normal}"
+}
+
 # Clone the PKGBUILD repository
+print_message1 "Cloning PKGBUILD repository"
 git clone -b 2-testing https://github.com/tcet-opensource/tcet-linux-pkgbuild.git 
 cd tcet-linux-pkgbuild/apps/tcet-linux-welcome
 
@@ -12,14 +29,14 @@ updatedRel=$((pkgrel + 1))
 
 # Update the PKGBUILD file with the new pkgrel value
 sed -i "s/^pkgrel=$pkgrel/pkgrel=$updatedRel/" PKGBUILD
-echo "Updated PKGBUILD:"
+print_message1 "Updated PKGBUILD:"
 cat PKGBUILD
 
-echo "makepkg started"
+print_message1 "Running makepkg"
 makepkg -s
 
 # Clone the testing repository
-echo "Cloning the testing repository"
+print_message1 "Cloning the testing repository"
 git clone https://github.com/tcet-opensource/tcet-linux-repo-testing.git
 
 # Set the file names and directory paths
@@ -38,41 +55,41 @@ done
 cp "$new_file" "$destination"
 
 # Update the repository database
-echo "Updating the repository database"
+print_message1 "Updating the repository database"
 cd tcet-linux-repo-testing/x86_64/
 ./update_repo.sh
 
 
 # Push to tcet-linux-repo-testing
-echo "Pushing to tcet-linux-repo-testing"
+print_message1 "Pushing to tcet-linux-repo-testing"
 cd ..
 git add .
 git remote set-url origin git@github.com:tcet-opensource/tcet-linux-repo-testing.git
 
 # Prompt the user for a commit message
-echo -n "Enter commit message: "
+echo -n "${bold}${yellow}Enter commit message:${normal} "
 read commit_message
 git commit -S -m "$commit_message"
 git push
 
 # Clean up the repository
-echo "Removing tcet-linux-repo"
+print_message1 "Removing tcet-linux-repo"
 cd ..
 rm -rf tcet-linux-repo-testing*
 
 # Clean up the PKGBUILD repository
-echo "Cleaning up PKGBUILD"
+print_message2 "Cleaning up PKGBUILD"
 ./cleanup.sh
 
 # Update the PKGBUILD repository
-echo "Updating PKGBUILD repository"
+print_message1 "Updating PKGBUILD repository"
 git add .
 git remote set-url origin git@github.com:tcet-opensource/tcet-linux-pkgbuild.git
 # Prompt the user for a commit message
-echo -n "Enter commit message: "
+echo -n "${bold}${yellow}Enter commit message:${normal} "
 read commit_message
 git commit -S -m "$commit_message"
 git push
 
-echo "PKGBUILD repository has been updated"
+print_message1 "PKGBUILD repository has been updated"
 
