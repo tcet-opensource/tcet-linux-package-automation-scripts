b1367208b98cc806ef986d0e6278de7cb14e0703
Akash6222 <akashdubey25274@gmail.com>
2023-08-27 14:09:07 +0530

    added functionn for server

--- automate.sh
+++ automate.sh
@@ -16,6 +16,9 @@ print_message2() {
     echo "${bold}${green}$1${normal}"
 }
 
+
+
+
 # Clone the PKGBUILD repository
 print_message1 "Cloning PKGBUILD repository"
 git clone https://github.com/tcet-opensource/tcet-linux-pkgbuild.git 
@@ -72,6 +75,62 @@ print_message1 "Running makepkg"
 makepkg -s
 
 
+
+# Function to handle repository operations
+handle_repository() {
+    local server=$1
+    local directory_name=$2
+
+    # Clone the repository
+    print_message1 "Cloning the $server"
+    git clone https://github.com/tcet-opensource/$server.git
+
+    # Set the file names and directory paths
+    new_file="$directory_name-$current_year.$current_month-$updatedRel-x86_64.pkg.tar.zst"
+    destination="$server/x86_64/"
+
+    # Remove the previous .zst file(s)
+    old_files="$destination"$directory_name-*zst
+    for file in $old_files; do
+        if [ -e "$file" ]; then
+            rm "$file"
+        fi
+    done
+
+    # Copy the new file to the destination
+    cp "$new_file" "$destination"
+
+    # Update the repository database
+    print_message1 "Updating the repository database"
+    cd $server/x86_64/
+    ./update_repo.sh
+
+    # Push to server
+    print_message1 "Pushing to $server"
+    cd ..
+    git add .
+    git remote set-url origin git@github.com:tcet-opensource/$server.git
+
+    # Prompt the user for a commit message
+    echo -n "${bold}${yellow}Enter commit message:${normal} "
+    read commit_message
+    git commit -S -m "$commit_message"
+
+    # Attempt to push and check the exit status
+    if ! git push; then
+        echo "Git push failed."
+        exit 0
+    fi
+
+    # Clean up the repository
+    print_message1 "Removing $server"
+    cd ..
+    rm -rf $server*
+}
+
+
+
+
 # Choose server repo
 print_message2 "Choose which repo you wanna clone and push"
 print_message1 "1) tcet-linux-applications"
@@ -83,63 +142,24 @@ read -p "Enter the number of your choice: " choice
 
 # Map choices to http
 case $choice in
-    1) server="tcet-linux-applications" ;;
-    2) server="tcet-linux-repo" ;;
+    1) server="tcet-linux-applications" 
+       handle_repository $server $directory_name
+       ;;
+    2) server="tcet-linux-repo" 
+       handle_repository $server $directory_name
+       ;;
     3) 
         server="tcet-linux-applications"
-        git clone https://github.com/tcet-opensource/$server.git
+        handle_repository $server $directory_name
         server="tcet-linux-repo"
-        git clone https://github.com/tcet-opensource/$server.git
+        handle_repository $server $directory_name
         ;;
     *) echo "Invalid choice"
        exit 1 ;;
 esac
 
-# Clone the testing repository
-print_message1 "Cloning the $server"
-git clone https://github.com/tcet-opensource/$server.git
-
-# Set the file names and directory paths
-new_file="$directory_name-$current_year.$current_month-$updatedRel-x86_64.pkg.tar.zst"
-destination="$server/x86_64/"
-
-# Remove the previous .zst file(s)
-old_files="$destination"$directory_name-*zst
-for file in $old_files; do
-    if [ -e "$file" ]; then
-        rm "$file"
-    fi
-done
-
-# Copy the new file to the destination
-cp "$new_file" "$destination"
-
-# Update the repository database
-print_message1 "Updating the repository database"
-cd $server/x86_64/
-./update_repo.sh
 
 
-# Push to server
-print_message1 "Pushing to $server"
-cd ..
-git add .
-git remote set-url origin git@github.com:tcet-opensource/$server.git
-
-# Prompt the user for a commit message
-echo -n "${bold}${yellow}Enter commit message:${normal} "
-read commit_message
-git commit -S -m "$commit_message"
-# Attempt to push and check the exit status
-if ! git push; then
-    echo "Git push failed."
-    exit 0
-fi
-
-# Clean up the repository
-print_message1 "Removing $server"
-cd ..
-rm -rf $server*
 
 # Clean up the PKGBUILD repository
 print_message2 "Cleaning up PKGBUILD"
