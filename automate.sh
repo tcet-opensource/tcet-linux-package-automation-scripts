ff4d9c606df2e49cfe7315982886368d56651630
Akash6222 <akashdubey25274@gmail.com>
2023-08-27 13:56:07 +0530

    fixing both option for server

--- automate.sh
+++ automate.sh
@@ -71,13 +71,37 @@ cat PKGBUILD
 print_message1 "Running makepkg"
 makepkg -s
 
+
+# Choose server repo
+print_message2 "Choose which repo you wanna clone and push"
+print_message1 "1) tcet-linux-applications"
+print_message1 "2) tcet-linux-repo"
+print_message1 "3) both"
+
+# Prompt the user for a choice
+read -p "Enter the number of your choice: " choice
+
+# Map choices to http
+case $choice in
+    1) server="tcet-linux-applications" ;;
+    2) server="tcet-linux-repo" ;;
+    3) 
+        server="tcet-linux-applications"
+        git clone https://github.com/tcet-opensource/$server.git
+        server="tcet-linux-repo"
+        git clone https://github.com/tcet-opensource/$server.git
+        ;;
+    *) echo "Invalid choice"
+       exit 1 ;;
+esac
+
 # Clone the testing repository
-print_message1 "Cloning the testing repository"
-git clone https://github.com/tcet-opensource/tcet-linux-applications.git
+print_message1 "Cloning the $server"
+git clone https://github.com/tcet-opensource/$server.git
 
 # Set the file names and directory paths
 new_file="$directory_name-$current_year.$current_month-$updatedRel-x86_64.pkg.tar.zst"
-destination="tcet-linux-repo-testing/x86_64/"
+destination="$server/x86_64/"
 
 # Remove the previous .zst file(s)
 old_files="$destination"$directory_name-*zst
@@ -92,15 +116,15 @@ cp "$new_file" "$destination"
 
 # Update the repository database
 print_message1 "Updating the repository database"
-cd tcet-linux-repo-testing/x86_64/
+cd $server/x86_64/
 ./update_repo.sh
 
 
-# Push to tcet-linux-repo-testing
-print_message1 "Pushing to tcet-linux-repo-testing"
+# Push to server
+print_message1 "Pushing to $server"
 cd ..
 git add .
-git remote set-url origin git@github.com:tcet-opensource/tcet-linux-repo-testing.git
+git remote set-url origin git@github.com:tcet-opensource/$server.git
 
 # Prompt the user for a commit message
 echo -n "${bold}${yellow}Enter commit message:${normal} "
@@ -113,9 +137,9 @@ if ! git push; then
 fi
 
 # Clean up the repository
-print_message1 "Removing tcet-linux-repo"
+print_message1 "Removing $server"
 cd ..
-rm -rf tcet-linux-repo-testing*
+rm -rf $server*
 
 # Clean up the PKGBUILD repository
 print_message2 "Cleaning up PKGBUILD"
