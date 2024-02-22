2c1ccb4d76cd9107e8d22378fe7a9826285048e0
Akash6222 <akashdubey25274@gmail.com>
2024-02-22 10:20:41 +0530

    error handling in update_server.sh

--- update_server.sh
+++ update_server.sh
@@ -5,31 +5,41 @@ update_server() {
     local server=$1
     local package_name=$PACKAGE_NAME
 
-    print_message1 "Updating the repository database"
-    cp -r $destination/* $server/x86_64/
-    cd $server/x86_64/
-    ./update_repo.sh
+    # Check if the server directory exists
+    if [ -d "$server" ]; then
+        print_message1 "Repository '$server' exists"
+        
+        # Update the repository database
+        print_message1 "Updating the repository database"
+        cp -r $destination/* $server/x86_64/
+        cd $server/x86_64/
+        ./update_repo.sh
 
-    # Push to server
-    print_message1 "Pushing to $server"
-    cd ..
-    git add .
-    git remote set-url origin git@github.com:tcet-opensource/$server.git
+        # Push to server
+        print_message1 "Pushing to $server"
+        cd ..
+        git add .
 
-    # Create commit message
+        # Set the remote URL
+        git remote set-url origin "git@github.com:tcet-opensource/$server.git"
 
-    if [ "$ans" == "yes" ]; then
-    commit_message="[PKG-UPD] All Packages Are Updated"
+        # Create commit message
+        if [ "$ans" == "yes" ]; then
+            commit_message="[PKG-UPD] All Packages Are Updated"
+        else
+            commit_message="[PKG-UPD] $package_name"    
+        fi
+
+        # Commit changes
+        git commit -S -m "$commit_message"
+
+        # Attempt to push and check the exit status
+        if ! git push; then
+            print_message4 "Git push failed."
+            perform_cleanup
+        fi
+        cd ..
     else
-    commit_message="[PKG-UPD] $package_name"    
+        print_message3 "Repository '$server' not found."
     fi
-
-    # Commit changes
-    git commit -S -m "$commit_message"
-    # Attempt to push and check the exit status
-    if ! git push; then
-        print_message4 "Git push failed."
-        perform_cleanup
-    fi
-    cd ..
 }
